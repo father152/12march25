d1a158e01814b52b404c96fef6b43db3d8f65174
Your Name <tsimbalenko.vv@gmail.com>
2025-03-20 13:04:58 +0200

    56

--- src/Components/Header/Contact/Contact.jsx
+++ src/Components/Header/Contact/Contact.jsx
@@ -1,10 +1,31 @@
-import React from 'react'
+import React from "react";
+import "./contact.css"
+import cheef from "./58fac1f040e874d2df9177fb34fcde60 (1).jpg";
+
+function Staff(props) {
+  return (
+    <div className="contact-card">
+      <img className="pic" src={props.image} alt={props.names} />
+      <span className="name">{props.name}</span>
+      <span className="phone">{props.phone}</span>
+    </div>
+  );
+}
 
 function Contact() {
   return (
-    <div className='contact'>
-    <h2>Contact</h2>
+    <div className="contact">
+      <h2>Contact</h2>
+      <div className="contact-title">
+        <Staff image={cheef} name="Pit Cgoter" phone="+56 45654 1321 54" />
+      </div>
+      <div className="contact-title">
+        <Staff image={cheef} name="Pit Cgoter" phone="+56 45654 1321 54" />
+      </div>
+      <div className="contact-title">
+        <Staff image={cheef} name="Pit Cgoter" phone="+56 45654 1321 54" />
+      </div>
     </div>
-  )
+  );
 }
-export {Contact}
+export { Contact };
--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -1,20 +1,23 @@
-import React from 'react'
-import "./main.css"
-import blockimg1 from "../img/af6544a1cd327d38ebe07fe40db1fabb.jpg"
-
-
+import React from "react";
+import "./main.css";
+import blockimg1 from "../img/af6544a1cd327d38ebe07fe40db1fabb.jpg";
 
 function Main() {
   return (
-    <div className='main'>
-    <h2>Main</h2>
-    <div className='container-main'>
-    <div className='block-1'>
-    <img src={blockimg1} alt=""></img>
+    <div className="main">
+      <h2>Main</h2>
+      <div className="container-main">
+        <div className="block-1">
+
+
+          <div className="girl">
+          <img src={blockimg1} alt=""></img>
+          </div>
+
+          <h1>never give up</h1>
+        </div>
+      </div>
     </div>
-    
-    </div>
-    </div>
-  )
+  );
 }
-export {Main}
+export { Main };
