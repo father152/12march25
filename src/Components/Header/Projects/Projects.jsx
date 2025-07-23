3dda2345dcb4d0287b084ff2d8cc1c16ad6dab2b
Your Name <tsimbalenko.vv@gmail.com>
2025-07-23 08:09:00 +0300

    45

--- src/Components/Header/Projects/Projects.jsx
+++ src/Components/Header/Projects/Projects.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import LoremIpsum from "react-lorem-ipsum";
 import "./projects.css";
 
+
+
+
 function Projects() {
   return (
     <div className="projects">
@@ -10,12 +13,15 @@ function Projects() {
         <h3>Title</h3>
         <p>
           <LoremIpsum />
+          <h4>First</h4>
+          <LoremIpsum />
+          <h4>Second</h4>
           <LoremIpsum />
           <LoremIpsum />
+          <h4>Third</h4>
           <LoremIpsum />
           <LoremIpsum />
-          <LoremIpsum />
-          <LoremIpsum />
+          
         </p>
       </div>
     </div>
