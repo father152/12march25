3b9f04d908ed83ec7128814335ffb3392a2f03ed
Your Name <tsimbalenko.vv@gmail.com>
2025-08-07 12:52:45 +0300

    45

--- src/Components/Header/About/About.jsx
+++ src/Components/Header/About/About.jsx
@@ -1,9 +1,10 @@
-import React from 'react'
 import "./about.css"
+
+
 function About() {
   return (
     <div className='about'>
-    <h2>About</h2>
+    <h2>Steps of our path</h2>
     </div>
   )
 }
